4288c1002752a8c597540b7bcebb9c79848d1fa8
Noufal Babu <noufal@scientiaindia.com>
2019-07-03 14:11:21 +0530

    Completed 5, added chant page

--- kivyapp2.py
+++ kivyapp2.py
@@ -7,11 +7,36 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+
 import socket_client
 import os
 import sys
 
 kivy.require("1.11.1")
+
+
+class ScrollableLabel(ScrollView):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.layout = GridLayout(cols=1, size_hint_y=None)
+        self.add_widget(self.layout)
+
+        self.chat_history = Label(size_hint_y=None, markup=True)
+        self.scroll_to_point = Label()
+
+        self.layout.add_widget(self.chat_history)
+        self.layout.add_widget(self.scroll_to_point)
+
+    def update_chat_history(self, message):
+        self.chat_history.text += '\n' + message
+
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.layout.chat_history.height = self.chat_history.texture_size[1]
+        self.layout.chat_history.text_size = (self.chat_history.width*0.98, None)
+
+        self.scroll_to(self.scroll_to_point)
 
 
 class ConnectPage(GridLayout):
@@ -20,7 +45,7 @@ class ConnectPage(GridLayout):
         self.cols = 2
 
         self.add_widget(Label(text='IP'))
-        self.ip = TextInput(multiline=False)
+        self.ip = TextInput(text='127.0.0.1', multiline=False)
         self.add_widget(self.ip)
 
         self.add_widget(Label(text='PORT'))
@@ -28,7 +53,7 @@ class ConnectPage(GridLayout):
         self.add_widget(self.port)
 
         self.add_widget(Label(text='Username'))
-        self.username = TextInput(multiline=False)
+        self.username = TextInput(text='sentdex',multiline=False)
         self.add_widget(self.username)
 
         self.join = Button(text='Join')
@@ -73,8 +98,22 @@ class InfoPage(GridLayout):
 class ChatPage(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='Hey we are here'))
+        self.cols = 1
+        self.rows = 2
+        self.history = Label(height=Window.size[1]*0.9,size_hint_y=None)
+        self.add_widget(self.history)
+
+        self.new_message = TextInput(width=Window.size[1]*0.9,size_hint_x=None,multiline=False)
+        self.send = Button(text='Send')
+        self.send.bind(on_press=self.send_message)
+
+        bottom_line = GridLayout(cols=2)
+        bottom_line.add_widget(self.new_message)
+        bottom_line.add_widget(self.send)
+        self.add_widget(bottom_line)
+
+    def send_message(self,_):
+        print('send a message')
 
 
 class EpicApp(App):
